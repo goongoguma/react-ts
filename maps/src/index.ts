bbf54ba2f0e9dd64764392a01df14ea2659d7cfa
jaehyun <wogus7an@gmail.com>
2020-07-21 22:26:08 +0900

    ts공부 12일차(googlemap with interface)

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,10 @@
-// import { User } from './User';
-// import { Company } from './Company';
+import { User } from './User';
+import { Company } from './Company';
+import { CustomMap } from '../src/CustomMap';
 
-const map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-});
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user)
+customMap.addCompanyMarker(company)
